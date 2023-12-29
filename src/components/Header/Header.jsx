de8beeaa2c6214ba70382021579382e0e9789376
Aisha Atta <aisha.atta1@gmail.com>
2023-12-29 00:13:13 -0800

    build header with temporary data

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,39 @@
-import React from 'react'
-import './Header.scss'
+import React from 'react';
+import './Header.scss';
+import logo from '../../assets/images/logo.png';
+import avatar from '../../assets/images/avatar.jpg';
+import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
+import { Link } from 'react-router-dom';
+import PersonOutlineOutlined from '@mui/icons-material/PersonOutlineOutlined';
 
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div className="header">
+            <div className="header__logo">
+                <Link to="/" className='header__ref'>
+                    <img 
+                    src={logo} 
+                    alt="logo"
+                    className='header__logo-image' />
+                </Link>
+               
+            </div>
+
+            <div className="header__items">
+                <PersonOutlineOutlined />
+                <DashboardOutlinedIcon />
+  
+                <div className="header__user">
+                    <img
+                        src={avatar}
+                        alt="profile avatar"
+                        className='header__avatar'
+                    />
+                    <p>Aisha Atta</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 function Home() {
   return (
-    <div>Home</div>
+    <div>
+
+      <div>Home</div>
+    <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aspernatur harum voluptatibus, veniam vel dolorem est repudiandae eum eos voluptatum iste nihil. Sunt nesciunt quasi quam labore officiis. Voluptas, sed.</div> <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aspernatur harum voluptatibus, veniam vel dolorem est repudiandae eum eos voluptatum iste nihil. Sunt nesciunt quasi quam labore officiis. Voluptas, sed.</div> <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aspernatur harum voluptatibus, veniam vel dolorem est repudiandae eum eos voluptatum iste nihil. Sunt nesciunt quasi quam labore officiis. Voluptas, sed.</div> <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aspernatur harum voluptatibus, veniam vel dolorem est repudiandae eum eos voluptatum iste nihil. Sunt nesciunt quasi quam labore officiis. Voluptas, sed.</div> <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur aspernatur harum voluptatibus, veniam vel dolorem est repudiandae eum eos voluptatum iste nihil. Sunt nesciunt quasi quam labore officiis. Voluptas, sed. Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium aspernatur impedit debitis porro! Quisquam assumenda quibusdam nesciunt vero minus nostrum officia tenetur eum quia. Explicabo distinctio perferendis facere odit odio! Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur maiores quos eligendi quo quis aspernatur sequi doloremque itaque perferendis, ipsum porro optio eos reprehenderit pariatur laborum. Libero laudantium incidunt non! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Optio, minima vel modi dolorem quibusdam rem eum, est vero maxime, praesentium nesciunt laboriosam aliquam deleniti magni voluptatem sint dolor quidem distinctio?</div>
+    </div>
+    
   )
 }
 
