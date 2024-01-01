90b95daf88e0030228a2972d566f40956b5666a1
Aisha Atta <aisha.atta1@gmail.com>
2024-01-01 01:34:15 -0800

    connect client with server to register new users

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,32 +1,88 @@
-import React from 'react'
+import { useState, useEffect } from 'react'
 import "./Register.scss";
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function Register() {
+    const URL_PATH = 'http://localhost:8080';
+    const navigate = useNavigate();
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: "",
+        name: "",
+    });
+
+    const [error, setError] = useState(null);
+    const [successMessage, setSuccessMessage] = useState(null);
+
+    const ErrorMessage = ({ error }) => {
+        return <div className="register__error">{error}</div>;
+    };
+
+    const handleChange = (e) => {
+        setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
+    };
+
+    const handleClick = async (e) => {
+        e.preventDefault();
+
+        if (!inputs.username || !inputs.password || !inputs.name || !inputs.email) {
+            setError("Please fill in all fields.");
+            return;
+        }
+
+        try {
+            await axios.post(`${URL_PATH}/api/auth/register`, inputs);
+            setError(null);
+            setSuccessMessage("Registration successful! Redirecting to login page...");
+
+            setTimeout(() => {
+                navigate('/login');
+            }, 2000);
+
+        } catch (err) {
+            setError(err.response.data);
+        }
+    };
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setSuccessMessage(null);
+        }, 5000);
+
+        return () => clearTimeout(timer);
+    }, [successMessage]);
+
+
     return (
         <div className='register'>
             <div className="register__container">
 
-            <div className="register__register">    
-                <h1 className='register__title'>Register</h1>
-                <form className='register__form'>
-                    <input type="text" placeholder='Username' />
-                    <input type="email" placeholder='Email' />
-                    <input type="password" placeholder='Password' />
-                    <input type="text" placeholder='Name' />
-                    <button className='register__button'>Register</button>
-                </form>
-</div>
+                <div className="register__register">
+                    <h1 className='register__title'>Register</h1>
+
+                    <form className='register__form'>
+                        {error && <div className="register__error">{error}</div>}
+                        {successMessage && <div className="register__success">{successMessage}</div>}
+                        <input type="text" placeholder='Username' name='username' onChange={handleChange} required />
+                        <input type="password" placeholder='Password' name='password' onChange={handleChange} required />
+                        <input type="text" placeholder='Name' name='name' onChange={handleChange} required />
+                        <input type="email" placeholder='Email' name='email' onChange={handleChange} required />
+                        <button className='register__button' onClick={handleClick}>Register</button>
+                    </form>
+
+                </div>
                 <div className="register__login">
-                    
-                        <h1 className='register__title'>Commit Hub</h1>
-                        <p className='register__text'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        <p className='register__sub-text'>I already have an account</p>
-                        <Link to="/login">
+
+                    <h1 className='register__title'>Welcome Back!</h1>
+                    <p className='register__text'>Embark on a journey to turn your dreams into achievements. Login to make it happen!</p>
+                    <p className='register__sub-text'>I already have an account</p>
+                    <Link to="/login">
                         <button className='register__button'>Log in</button>
-                        </Link>
-                        
-                    
+                    </Link>
+
+
                 </div>
             </div>
 
