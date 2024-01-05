71730002863753a8b152ef19f3582901791e4dab
Aisha Atta <aisha.atta1@gmail.com>
2024-01-04 23:20:54 -0800

    update user information on the nav bar

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -2,14 +2,15 @@ import avatar from '../../assets/images/avatar.jpg';
 import './comments.scss';
 import { useQuery, useQueryClient, useMutation } from "react-query";
 import { apiRequest } from "../../utils/axios.jsx";
-import { useContext } from "react";
+import { useState, useContext } from "react";
 import { AuthContext } from "../../context/authentication.jsx";
 import moment from "moment";
 
-function Comments({goalId}) {
-      
+function Comments({ goalId }) {
+    const [addComment, setAddComment] = useState('');
     const { currentUser } = useContext(AuthContext);
-      
+    const queryClient = useQueryClient();
+
     const { isLoading, error, data: commentsData } = useQuery(["comments", goalId], () =>
         apiRequest.get(`/comments?goalId=${goalId}`).then((response) => {
             console.log("Response: ", response.data)
@@ -23,28 +24,63 @@ function Comments({goalId}) {
 
     if (error) {
         console.error("Error fetching comments:", error);
-        return <p>Error fetching goals</p>;
+        return <p>Error fetching comments</p>;
     }
 
-  return (
-    <div className="comments">
-      <div className="comments__input">
-        <img src={avatar} alt="your avatar" className='comments__avatar'/>
-        <input type="text" placeholder="Join the conversation" />
-        <button className='comments__button'>Send</button>
-      </div>
-      {commentsData.map((comment) => (
-        <div key={comment.id}  className="comments__comment">
-          <img src={comment.avatar} alt="user avatar" className='comments__avatar' />
-          <div className="comments__detail">
-            <p className="comments__name">{comment.userName}</p>
-            <p className="comments__comment-actual">{comment.comment}</p>
-          </div>
-          <span className="comments__date">{moment(comment.created_at).fromNow()}</span>
+    const mutation = useMutation(
+        (newComment) => {
+            return apiRequest.post("/comments", newComment);
+        },
+        {
+            onSuccess: () => {
+                queryClient.invalidateQueries(["comments"]);
+            }
+        }
+    )
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        if (!addComment.trim()) {
+            //Add Error message - Validation
+            return;
+        }
+
+        try {
+            const newComment = {
+                comment: addComment,
+                user_comment_id: currentUser.id,
+                goals_comment_id: goalId,
+                created_at: moment(Date.now()).format("YYYY-MM-DD HH:mm:ss"),
+            };
+
+            await mutation.mutate(newComment);
+            setAddComment('');
+        } catch (error) {
+            console.error("Error creating comment:", error);
+        }
+    };
+
+    return (
+        <div className="comments">
+            <div className="comments__input">
+                <img src={avatar} alt="your avatar" className='comments__avatar' />
+                <input type="text" placeholder="Join the conversation" value={addComment} onChange={(e) => setAddComment(e.target.value)} />
+                <button className='comments__button' onClick={handleSubmit}>Send</button>
+            </div>
+
+            {commentsData.map((comment) => (
+                <div key={comment.id} className="comments__comment">
+                    <img src={comment.avatar} alt="user avatar" className='comments__avatar' />
+                    <div className="comments__detail">
+                        <p className="comments__name">{comment.userName}</p>
+                        <p className="comments__comment-actual">{comment.comment}</p>
+                    </div>
+                    <span className="comments__date">{moment(comment.created_at).fromNow()}</span>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  )
+    )
 }
 
 export default Comments
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import { useContext } from 'react';
 import './Header.scss';
 import logo from '../../assets/images/logo.png';
-import avatar from '../../assets/images/avatar.jpg';
+//import avatar from '../../assets/images/avatar.jpg';
 import DashboardOutlinedIcon from '@mui/icons-material/DashboardOutlined';
 import { Link } from 'react-router-dom';
 import PersonOutlineOutlined from '@mui/icons-material/PersonOutlineOutlined';
+import { AuthContext } from "../../context/authentication.jsx";
 
 function Header() {
+    const { currentUser } = useContext(AuthContext);
+
     return (
         <div className="header">
             <div className="header__logo">
@@ -25,11 +28,11 @@ function Header() {
   
                 <div className="header__user">
                     <img
-                        src={avatar}
+                        src={currentUser.avatar}
                         alt="profile avatar"
                         className='header__avatar'
                     />
-                    <p>Aisha Atta</p>
+                    <p>{currentUser.name}</p>
                 </div>
             </div>
         </div>
