3e6fb39f807d115a9b2d113cd0ca6fcc479c62ed
Aisha Atta <aisha.atta1@gmail.com>
2024-01-06 00:50:42 -0800

    create handle refresh function and modify the useEffect

--- src/components/Quote/Quote.jsx
+++ src/components/Quote/Quote.jsx
@@ -2,39 +2,44 @@ import './Quote.scss';
 import { useEffect, useState } from 'react';
 import axios from "axios";
 import FormatQuoteIcon from '@mui/icons-material/FormatQuote';
-import Divider from '@mui/material/Divider';
 import RefreshIcon from '@mui/icons-material/Refresh';
 
 
 function Quote() {
 
     const [quote, setQuote] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
     const URL_PATH = "https://api.quotable.io/random?maxLength=100&tags=%22inspirational%22%7C+%22wisdom%22";
 
-    useEffect(() => {
-        
-            const fetchQuote = async () => {
-            try {   
-                 const result = await axios.get(URL_PATH);
-                setQuote(result.data);
-                console.log(setQuote);
-            }
-            
-         catch (err) {
+    const fetchQuote = async () => {
+        try {
+            setIsLoading(true);
+            const result = await axios.get(URL_PATH);
+            setQuote(result.data);
+        } catch (err) {
             console.error("Error fetching quote: ", err);
-        } }
-fetchQuote();
-    }, []);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const handleRefresh = (e) => {
+        e.preventDefault();
+        fetchQuote();
+    }
+
+    useEffect(() => {
+        fetchQuote();
+    }, []); // Fetch quote on mount
 
     return (
         <div className='quote'>
-            <div className="quote__container">
-                <FormatQuoteIcon className='quote__icon'/>
+            <div className={`quote__container ${isLoading ? 'loading' : ''}`}>
+                <FormatQuoteIcon className='quote__icon' />
                 <h4 className='quote__quote'>{quote.content}</h4>
-                <hr className='quote__divider'/>
-                {/* <Divider className='quote__divider'/> */}
+                <hr className='quote__divider' />
                 <p className='quote__author'>{quote.author}</p>
-                <RefreshIcon className='quote__refresh'/>
+                <RefreshIcon className='quote__refresh' onClick={handleRefresh} />
             </div>
 
         </div>
