a2ac43e4058095900fe517d2f650f272ff1d0bc1
Aisha Atta <aisha.atta1@gmail.com>
2024-01-06 23:57:57 -0800

    create profile page with integration

---
+++ src/components/GoalsByUser/GoalsByUser.jsx
@@ -0,0 +1,53 @@
+import './GoalsbyUser.scss';
+import { useQuery } from "react-query";
+import { apiRequest } from "../../utils/axios.jsx";
+import moment from "moment";
+import Interactions from '../Interactions/Interactions';
+
+function GoalsByUser({id}) {
+    const { isLoading: goalsLoading, error: goalsError, data: goalsData } = useQuery(["goals"], () =>
+        apiRequest.get(`/goals/user/${id}`).then((response) => {
+            console.log("Response to Goals by User: ", response.data)
+            return response.data;
+        })
+    );
+
+    if (goalsLoading) {
+        return <p>Loading...</p>;
+    }
+
+    if (goalsError) {
+        console.error("Error fetching goals:", goalsError);
+        return <p>Error fetching goals</p>;
+    }
+
+  return (
+    <div className='goalsByUser'>
+            <h2 className='goalsByUser__title'>goalsByUser Feed</h2>
+            <div className="goalsByUser__list">
+                {goalsData.map((goalData) => (
+                    <li key={goalData.id} className='goalsByUser__item'>
+                        <div className='goalsByUser__user'>
+                            <img src={goalData.avatar} alt="default" className="goalsByUser__avatar" />
+                            <div className='goalsByUser__user-info'>
+                                <span className="goalsByUser__name">{goalData.userName}</span>
+                                <span className="goalsByUser__date">{moment(goalData.created_at).fromNow()}</span>
+                            </div>
+                        </div>
+
+                        <div className="goalsByUser__content">
+                            <p className='goalsByUser__description'>{goalData.description}</p>
+                            <img src={goalData.image} alt="commitment image" className='goalsByUser__image' />
+                        </div>
+                        
+                        < Interactions goalId={goalData.id} />
+                        
+                    </li>
+                ))}
+
+            </div>
+        </div>
+  )
+}
+
+export default GoalsByUser
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -1,9 +1,45 @@
-import React from 'react';
+import { useEffect, useState } from 'react';
 import "./Profile.scss";
+import avatar from '../../assets/images/avatar.jpg';
+import { useParams } from 'react-router-dom';
+import { apiRequest } from '../../utils/axios.jsx';
+import Commitments from '../../components/Commitments/Commitments';
+import GoalsByUser from '../../components/GoalsByUser/GoalsByUser';
 
 function Profile() {
+  const { id } = useParams();
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const response = await apiRequest.get(`/users/profile/${id}`);
+        console.log("userData: ", response.data);
+        setUserData(response.data);
+      } catch (error) {
+        console.error('Error fetching user data:', error);
+      }
+    };
+
+    fetchUserData();
+  }, [id]);
+
+  if (!userData) {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <div>Profile</div>
+    <div className='profile'>
+      <div className="profile__info">
+        <img src={userData.avatar} alt='avatar' className='profile__avatar' />
+        <div className="profile__name">{userData.name}</div>
+      </div>
+
+      <div className="profile__contents">
+        Posts
+        <GoalsByUser id={id}/>
+      </div>
+    </div>
   )
 }
 
