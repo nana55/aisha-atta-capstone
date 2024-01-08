6318e1ac170be23558250812b45169ac6ccbb89c
Aisha Atta <aisha.atta1@gmail.com>
2024-01-07 17:40:04 -0800

    add UI for displaying error with Toastify on progress component

--- src/components/ProgressTracker/ProgressTracker.jsx
+++ src/components/ProgressTracker/ProgressTracker.jsx
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react'
 import './ProgressTracker.scss'
 import { apiRequest } from "../../utils/axios.jsx";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function ProgressTracker({ goalId }) {
     const [progress, setProgress] = useState(10);
@@ -10,13 +12,12 @@ function ProgressTracker({ goalId }) {
     }, [goalId]);
 
     const fetchProgressFromDatabase = async () => {
-        console.log('Fetching progress...');
         try {
             const response = await apiRequest.get('/progress');
             const goalProgress = response.data.find(item => item.goalId === goalId);
             setProgress(goalProgress ? goalProgress.progress : 10);
         } catch (error) {
-            console.error('Error fetching progress from the database:', error);
+            toast.error('Error fetching progress from the database');
         }
     };
 
@@ -24,35 +25,33 @@ function ProgressTracker({ goalId }) {
         try {
             const newPercentage = Math.min(progress + 10, 100);
             const response = await apiRequest.put('/progress', { percentage: newPercentage, goalId });
-            console.log('Server Response:', response.data);
             setProgress(newPercentage);
-            console.log('Updated Progress:', newPercentage);
         } catch (error) {
-            console.error('Error updating progress in the database:', error);
+            toast.error('Error updating progress in the database');
         }
     };
 
     const resetProgress = async () => {
         try {
             const response = await apiRequest.put('/progress', { percentage: 0, goalId });
-            console.log('Server Response:', response.data);
             setProgress(0);
         } catch (error) {
-            console.error('Error resetting progress in the database:', error);
+            toast.error('Error resetting progress in the database');
         }
     };
 
     return (
         <div className="progress-bar">
-      <div className="progress-bar__container">
-        <div className="progress-bar__fill" style={{ width: `${progress}%` }} />
-        <span className="progress-bar__percentage">{progress}%</span>
-      </div>
-      <div className="progress-bar__buttons">
-        <button className="progress-bar__update" onClick={updateProgress}>Update</button>
-        <button className="progress-bar__reset" onClick={resetProgress}>Reset</button>
-      </div>
-    </div>
+            <ToastContainer />
+            <div className="progress-bar__container">
+                <div className="progress-bar__fill" style={{ width: `${progress}%` }} />
+                <span className="progress-bar__percentage">{progress}%</span>
+            </div>
+            <div className="progress-bar__buttons">
+                <button className="progress-bar__update" onClick={updateProgress}>Update</button>
+                <button className="progress-bar__reset" onClick={resetProgress}>Reset</button>
+            </div>
+        </div>
     )
 }
 
