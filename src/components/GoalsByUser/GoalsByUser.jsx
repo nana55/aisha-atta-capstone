7939ef11a70eae12f212cccc7b364f5d6e2f1a7d
Aisha Atta <aisha.atta1@gmail.com>
2024-01-07 19:19:25 -0800

    replace console logs with toastify errors for GoalsbyUser component

--- src/components/GoalsByUser/GoalsByUser.jsx
+++ src/components/GoalsByUser/GoalsByUser.jsx
@@ -6,13 +6,14 @@ import Interactions from '../Interactions/Interactions';
 import ProgressTracker from '../ProgressTracker/ProgressTracker';
 import { AuthContext } from '../../context/authentication.jsx';
 import { useContext } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function GoalsByUser({ id }) {
     const { currentUser } = useContext(AuthContext);
 
     const { isLoading: goalsLoading, error: goalsError, data: goalsData } = useQuery(["goals"], () =>
         apiRequest.get(`/goals/user/${id}`).then((response) => {
-            console.log("Response to Goals by User: ", response.data)
             return response.data;
         })
     );
@@ -22,12 +23,13 @@ function GoalsByUser({ id }) {
     }
 
     if (goalsError) {
-        console.error("Error fetching goals:", goalsError);
-        return <p>Error fetching goals</p>;
+        toast.error("Error fetching goals");
+        return;
     }
 
   return (
     <div className='goalsByUser'>
+        <ToastContainer />
             <div className="goalsByUser__list">
                 {goalsData.map((goalData) => (
                     <li key={goalData.id} className='goalsByUser__item'>
@@ -43,7 +45,7 @@ function GoalsByUser({ id }) {
                             <p className='goalsByUser__description'>{goalData.description}</p>
                             
                             {goalData.image && (
-                                <img src={goalData.image} alt={`Image for ${goalData.description}`} className='commitments__image' />
+                                <img src={goalData.image} alt={`Image for ${goalData.description}`} className='goalsByUser__image' />
                             )}
                             {!goalData.image && (<img src='' alt='' />)}
                             
